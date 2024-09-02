fb71a640103cceccb41dcfafbffe5e0ebbfc680b
codingkunst <ryoo6931@gmail.com>
2024-09-02 20:40:51 +0900

    [Feat] 데이터 조회

--- src/pages/ProductAll.jsx
+++ src/pages/ProductAll.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import ProductCard from '../components/ProductCard'
 
 const ProductAll = () => {
+  const apiUrl = import.meta.env.VITE_SERVER_URL;
+
+  const [productList, setProductList] = useState([]);
+
+  const getProducts = async () => {
+    let url = `${apiUrl}/products`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log('data: ', data);
+    setProductList(data);
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, [])
+
   return (
-    <div>ProductAll</div>
+    <Container>
+      <Row>
+        {productList.map((item) => (
+          <Col lg={3}>
+            <ProductCard item={item} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   )
 }
 
