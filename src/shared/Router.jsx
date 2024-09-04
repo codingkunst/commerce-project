302642834888ab3c1bd769d89aeda9b298e314bb
codingkunst <ryoo6931@gmail.com>
2024-09-04 11:01:30 +0900

    [Feat] 임시 로그인 기능 구현

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,19 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import ProductAll from "../pages/ProductAll";
 import Login from "../pages/Login";
 import ProductDetail from "../pages/ProductDetail";
+import PrivateRoute from "../route/PrivateRoute";
 
 const Router = () => {
   const [authenticate, setAuthenticate] = useState(false);
+
+  useEffect(()=> {
+    console.log('aaaa', authenticate)
+  }, [authenticate])
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<ProductAll />} />
-        <Route path="login" element={<Login />} />
-        <Route path="product/:id" element={<ProductDetail />} />
+        <Route path="login" element={<Login setAuthenticate={setAuthenticate} />} />
+        <Route path="product/:id" element={<PrivateRoute authenticate={authenticate} />} />
       </Routes>
     </BrowserRouter>
   );
